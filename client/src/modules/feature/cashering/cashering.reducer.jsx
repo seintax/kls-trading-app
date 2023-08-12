276cf8a8dc4212ee521127578867836ce94833b5
seintax <78845146+seintax@users.noreply.github.com>
2023-08-12 16:46:54 +0800

    transaction update order

--- client/src/modules/feature/cashering/cashering.reducer.jsx
+++ client/src/modules/feature/cashering/cashering.reducer.jsx
@@ -18,8 +18,9 @@ const initialState = {
             { name: 'Transaction', stack: false, sort: 'code' },
             { name: 'Time', stack: true, sort: 'time', size: 200 },
             { name: 'Type', stack: true, sort: 'method', size: 150 },
-            { name: 'Status', stack: true, sort: 'status', size: 250 },
+            { name: 'Status', stack: true, sort: 'status', size: 180 },
             { name: 'Net Amount', stack: false, size: 200 },
+            { name: 'Customer', stack: false, size: 250 },
             { name: '', sort: 'account_store', stack: true, size: 120, position: 'right' },
             { name: '', stack: false, screenreader: 'Action', size: 70 }
         ]
