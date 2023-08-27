d4a28defbf0349bc72fb1ba2309fbd364baee376
seintax <78845146+seintax@users.noreply.github.com>
2023-08-27 11:53:12 +0800

    fix menu not displaying

--- client/src/modules/app/app.index.jsx
+++ client/src/modules/app/app.index.jsx
@@ -26,7 +26,7 @@ import AppSideMenu from "../../utilities/interface/application/navigation/app.si
 import NotificationContainer from "../../utilities/interface/notification/notification.container"
 import { defaultRole } from "../../utilities/variables/string.variables"
 import { useUpdateAccountMutation } from "../system/account/account.services"
-import { resetSettingsConfig, setSettingsConfig, setSettingsMenus, setSettingsNotifier } from "../system/config/config.reducer"
+import { setSettingsConfig, setSettingsMenus, setSettingsNotifier } from "../system/config/config.reducer"
 import { useByAccountConfigMutation } from "../system/config/config.services"
 import { setPermissionCache } from "../system/permission/permission.reducer"
 import { useFetchAllPermissionMutation } from "../system/permission/permission.services"
@@ -101,11 +101,16 @@ const AppIndex = () => {
     const [createRole] = useCreateRolesMutation()
     const [updateAccount] = useUpdateAccountMutation()
     const [accountConfig, { isSuccess }] = useByAccountConfigMutation()
+    const defaultConfig = {
+        discount: "Amount",
+        ratelimit: 100,
+        shownetdiscount: "Yes",
+        simplifiedcashering: "No"
+    }
 
     useEffect(() => {
         if (configSelector.updater) {
             dispatch(setSettingsMenus(menulist(configSelector.config)))
-            // setMenus(menulist(configSelector.config))
         }
     }, [configSelector.updater])
 
@@ -165,11 +170,12 @@ const AppIndex = () => {
                             let config = res.distinctResult.data[0]
                             dispatch(setSettingsConfig(JSON.parse(config.json)))
                             dispatch(setSettingsMenus((menulist(JSON.parse(config.json)))))
-                            // setMenus(menulist(JSON.parse(config.json)))
                             dispatch(setSettingsNotifier(true))
                         }
                         if (!res.distinctResult.data?.length) {
-                            dispatch(resetSettingsConfig())
+                            dispatch(setSettingsConfig(defaultConfig))
+                            dispatch(setSettingsMenus((menulist(defaultConfig))))
+                            dispatch(setSettingsNotifier(true))
                         }
                     }
                 })
