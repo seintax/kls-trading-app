dca1441507396efc4f7637f74cbd66d3b7f37936
seintax <78845146+seintax@users.noreply.github.com>
2023-10-02 07:47:13 +0800

    inventory error fixed

--- src/feature/inventory/inventory.query.js
+++ src/feature/inventory/inventory.query.js
@@ -121,8 +121,10 @@ const byProduct = handler(async (req, res) => {
     const param = getbranch.parameters(req.query)
     const { product, category, acquisition, store, stocks } = getbranch.fields
     const { variant_serial, variant_model, variant_brand } = getbranch.included
-    let params = [p(param.product).Exactly(), p(param.category).Exactly(), p(param.branch).Contains(), "0", "PROCUREMENT", "TRANSFER", "MIGRATION"]
-    let clause = [f(product).IsEqual(), f(category).IsEqual(), f(store).Like(), f(stocks).Greater(), f(acquisition).Either(["", "", ""])]
+    // let params = [p(param.product).Exactly(), p(param.category).Exactly(), p(param.branch).Contains(), "0", "PROCUREMENT", "TRANSFER", "MIGRATION"]
+    // let clause = [f(product).IsEqual(), f(category).IsEqual(), f(store).Like(), f(stocks).Greater(), f(acquisition).Either(["", "", ""])]
+    let params = [p(param.product).Exactly(), p(param.category).Exactly(), p(param.branch).Contains(), "0"]
+    let clause = [f(product).IsEqual(), f(category).IsEqual(), f(store).Like(), f(stocks).Greater()]
     let series = [f(variant_serial).Asc(), f(variant_model).Asc(), f(variant_brand).Asc()]
     let limits = undefined
     const builder = getbranch.inquiry(clause, params, series, limits)
