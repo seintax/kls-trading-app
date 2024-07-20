fa529785c3ca005f6611dd224a4b41d764126e92
seintax <78845146+seintax@users.noreply.github.com>
2024-07-21 02:03:10 +0800

    calibrate date for test endpoint

--- src/system/test/test.route.js
+++ src/system/test/test.route.js
@@ -4,7 +4,9 @@ const moment = require('moment')
 router.get('/test/connection', (req, res) => {
     res.status(200).json({
         message: "You are successfully connected",
-        on: moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
+        on: process.env.NODE_ENV === "development"
+            ? moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
+            : moment(new Date()).add(8, 'HOURS').format("YYYY-MM-DD HH:mm:ss")
     })
 })
 
